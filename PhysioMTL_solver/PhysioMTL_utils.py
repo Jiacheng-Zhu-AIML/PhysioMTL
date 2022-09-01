d6043a88a9de25ab74bbd372105fad1bdbd25407
jiachengzhu <jiachengzhu1994@gmail.com>
2022-09-01 14:28:33 -0400

    clean the file and add comments

--- PhysioMTL_solver/PhysioMTL_utils.py
+++ PhysioMTL_solver/PhysioMTL_utils.py
@@ -1,55 +1,17 @@
 """
-The utils functions for PhysioMTL
-Data generation, visualization and so on
+The utils functions for PhysioMTL solver and computation.
+Synthetic data generation, data processing, and visualization.
 """
 import matplotlib.cm as cm
 import numpy as np
-from matplotlib import pyplot as plt
-
-
-# def get_sin_data(M_str, A_str, phi_str, data_num, noise, t_raw):
-#     noise_raw = np.random.normal(loc=0, scale=noise, size=data_num)
-#     y_raw = M_str + A_str * np.sin(t_raw * 0.25 + phi_str) + noise_raw
-#
-#     return y_raw
-
-
-# [[t_i(np), f_i(np), s_i(np)],
-#  [t_i(np), f_i(np), s_i(np)],
-#  ....,]
-def get_rainbow_curves(s_list):
-    all_data_list = []
-    para_list = []
-
-    noise_index = 0.3
-    for index, s in enumerate(s_list):
-        t_i_raw = np.linspace(0, 25, 30) + np.random.normal(loc=0, scale=0.0, size=30)
-        noise_i_raw = np.random.normal(loc=0, scale=noise_index, size=30)
-        temp_para = [35 + s * 5, 3.0 + s / 5, (0.34) + s * 0.000, -1.8 + 0.4 * s]
-        print("temp_para =", temp_para)
-        y_i_raw = temp_para[0] + temp_para[1] * np.sin(t_i_raw * temp_para[2] + temp_para[3]) + noise_i_raw
-        all_data_list.append([t_i_raw, y_i_raw, s])
-
-        # Notice: Get the groundtruth linear parameter
-        #   y_i = A * sin (r * t_i + \phi) + m
-        #   y_i = theta_1 * x_i + theta_2 * z_i + theta_3
-        #   theta_1 = A * cos(\phi), theta_2 = A * sin(\phi), theta_3 = m
-        linear_para = [temp_para[1] * np.cos(temp_para[3]), temp_para[1] * np.sin(temp_para[3]), temp_para[0]]
-        para_list.append(linear_para)
-
-    return all_data_list, para_list
-
-
-#   Notice:
-#   [[t_i(np), f_i(np), s_i(np)],
-#   [t_i(np), f_i(np), s_i(np)],
-#   ....,]
-causual_map_default = np.array([[0.2, 3.0],
-                                [0.4, -1.8],
-                                [5, 35]])
 
 
 def underlying_truth(s):
+    """ The underlying procedure that generates function values
+    give a taskwise feature.
+    :param s: float, the task indicator
+    :return: The amplitude A, phase phi, and the vertical shift M.
+    """
     A = 0.2 * s + 3.0
     phi = 0.4 * s - 1.8
     M = 5 * s + 35
@@ -57,6 +19,16 @@ def underlying_truth(s):
 
 
 def get_rainbow_curves_new(s_list_input, data_noise=0.1, underlying_func=underlying_truth):
+    """
+    Generate the dataset for regression
+    :param s_list_input: list of , A list of taskwise features
+    :param data_noise: float, defaults to 0.1. The noise for data generation.
+    :param underlying_func: python function. The underlying sinusoidal function.
+    :return: A list of t for each task, for plotting.
+             A list of feature X for each task, for multitask regression.
+             A list of task indicator s for each task, for multitask regression.
+             A list of target X for each task, for multitask regression.
+    """
     t_list = []  # t index for plot
     x_list = []  # X feature for regression
     s_list = []  # task feature for regression
@@ -84,6 +56,14 @@ def get_rainbow_curves_new(s_list_input, data_noise=0.1, underlying_func=underly
 
 # Notice: Process data for training PhysioMTL
 def process_for_PhysioMTL(raw_t_list, raw_x_list, raw_s_list, raw_y_list):
+    """
+    Reshape the data for PhysioMTL solver. Reparamterize the taskwise indicators as
+    taskwise features.
+    :return: A list of t for each task, for plotting.
+             A list of feature X for each task, for multitask regression.
+             A list of taskwise feature S for each task, for multitask regression.
+             A list of target X for each task, for multitask regression.
+    """
     X_train_list = []
     S_train_list = []
     Y_train_list = []
@@ -93,7 +73,6 @@ def process_for_PhysioMTL(raw_t_list, raw_x_list, raw_s_list, raw_y_list):
         X_train_list.append(raw_x_list[task_i])
         # Notice: Y_vec: (30, ) -> (30, 1)
         Y_train_list.append(raw_y_list[task_i].reshape((-1, 1)))
-
         # Notice:! S:    number -> [s, sin(s), cos(s), 1].T  (4, 1)
         S_vec = np.array([[s_value],
                           [np.sin(s_value)],
@@ -107,6 +86,11 @@ def process_for_PhysioMTL(raw_t_list, raw_x_list, raw_s_list, raw_y_list):
 # Notice: process data for training other MTL
 #       https://github.com/hichamjanati/mutar
 def process_for_MTL(raw_t_list, raw_x_list, raw_s_list, raw_y_list):
+    """
+    Transform the PhysioMTL data into the format for other multitask regressions methods.
+    Combine the taskwise feature into the input feature X.
+    :return: X_mat, Y_mat
+    """
     task_num = len(raw_t_list)
 
     X_mat = np.zeros((task_num, 30, 4))
@@ -122,13 +106,27 @@ def process_for_MTL(raw_t_list, raw_x_list, raw_s_list, raw_y_list):
 
 # Notice: k_nearest for normal MTL generalization
 def k_nearest_list(value, list_input, k):
+    """
+    Given a target value, return top k nearest elements in a list.
+    :param value: float, a target value.
+    :param list_input: list, a list of values.
+    :param k: The number of top k elements.
+    :return: list of values.
+    """
     ans = [n for d, n in sorted((abs(x - value), x) for x in list_input)[:k]]
     return ans
 
 
 # Notice: get the model parameter mat
 def k_nearest_model_para(train_W, train_s_list, test_s_list, k=2):
-    w_dim, train_task_num = train_W.shape
+    """
+    Use the taskwise indicators to find the k nearest parameters.
+    :param train_W: The learned weight matrix from the training set (feature_d, train_task_num)
+    :param train_s_list: The list of task indicators for the training set.
+    :param test_s_list:The list of task indicators for the testing set.
+    :param k: The number of top k elements.
+    :return: (feature_d, test_task_num)
+    """
     model_para_mat_list = []
     for s_test in test_s_list:
         result_list = k_nearest_list(s_test, train_s_list, k)
@@ -142,6 +140,12 @@ def k_nearest_model_para(train_W, train_s_list, test_s_list, k=2):
 
 # Notice: get the RMSE error of two list of np
 def compute_list_rmse(list_a, list_b):
+    """
+    Compute the average RMSE error given to list of numpy.ndarrays.
+    :param list_a: List of ndarrays
+    :param list_b: List of ndarrays
+    :return: float
+    """
     if len(list_a) != len(list_b):
         print("Something is wrong")
     num_task = len(list_a)
@@ -155,28 +159,14 @@ def compute_list_rmse(list_a, list_b):
 
 
 # Notice: Visualize the task relation by rainbow color
-colors_f = cm.rainbow(0.1 * np.linspace(0, 10, 101))
-l_np = np.linspace(0, 10, 101)
-
-
 def get_rainbow_from_s(s):
+    """
+    Get the color coefficient from taskwise indicators.
+    :param s: float
+    :return: numpy.ndarray from cm.rainbow()
+    """
+    colors_f = cm.rainbow(0.1 * np.linspace(0, 10, 101))
+    l_np = np.linspace(0, 10, 101)
     color_select = min(list(l_np), key=lambda x: abs(x - s))
     color_index = list(l_np).index(color_select)
     return colors_f[color_index]
-
-
-# Notice # scatter data samples
-def scatter_data_with_s(plt, t_list_raw, Y_raw_list, S_raw_list, rainbow_func=get_rainbow_from_s, **kwargs):
-    for task_i, s_value in enumerate(S_raw_list):
-        plt.scatter(t_list_raw[task_i], Y_raw_list[task_i], label="task code:" + str(s_value),
-                    color=rainbow_func(s_value), **kwargs)
-        # plt.plot(t_test, pred_Y_list[task_i], label="pred" + str(s_value), color=get_rainbow_from_s(s_value))
-    return plt
-
-
-# Notice # plot
-def plot_data_curve_with_s(t_list_or_np, Y_list, S_raw_list, rainbow_func=get_rainbow_from_s, **kwargs):
-    if isinstance(t_list_or_np, list):
-        for task_i, s_value in enumerate(S_raw_list):
-            plt.plot(t_list_or_np[task_i], Y_list[task_i], label="pred" + str(s_value), color=rainbow_func(s_value),
-                     **kwargs)
